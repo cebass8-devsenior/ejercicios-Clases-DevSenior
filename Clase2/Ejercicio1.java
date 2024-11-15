c4fc0312f6f9976167fd7bcf15d811258c71d3cd
Giovanni Mauricio Ceballos Betancurth <cebass8@gmail.com>
2024-11-15 09:41:35 -0500

    Cambio

--- Clase2/Ejercicio1.java
+++ Clase2/Ejercicio1.java
@@ -1,9 +1,8 @@
 public class Ejercicio1{
     public static void main(String[] args){
-        int a = 6;
-        int b = 2*(1+2);
+        var resultado = 6/2*(1+2);
 
-        System.out.println("El resultado es: " + a/b);
+        System.out.println("El resultado es: " + resultado);
         // el resultado es uno porque la jerarquía es resolver primero el paréntesis, luego la multiplicación y por último la división.
     }
 }
